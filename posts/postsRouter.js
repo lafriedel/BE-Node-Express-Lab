ec19ba223de6e02be5a46a6daf2417f104aa8331
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-12 15:46:08 -0500

    Successful GET at /api/posts

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -4,4 +4,15 @@ const Posts = require('../data/db');
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    Posts
+        .find()
+        .then(posts => {
+            res.status(200).json({ posts });
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The posts information could not be retrieved." });
+        });
+});
+
 module.exports = router;
