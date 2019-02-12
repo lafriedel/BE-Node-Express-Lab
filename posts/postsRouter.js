9b8418aa4335db34b6931fff111b22614e6ff195
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-12 15:52:04 -0500

    Successful GET at /api/posts/:id

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -15,4 +15,21 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    Posts
+        .findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json({ post });
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        });
+})
+
 module.exports = router;
